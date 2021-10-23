745130fb62533bd7c195df4ce2cb3efaeb968306
MUKUL NAGPAL <mukulnagpal1993@gmail.com>
2021-10-23 07:20:14 +0530

    final

---
+++ counter/app.js
@@ -0,0 +1,61 @@
+
+let increased = document.querySelector('#increase')
+let container = document.querySelector('#container')
+let decreased = document.querySelector('#decrease')
+let reset = document.querySelector('#reset')
+let count = 0
+
+
+
+increased.addEventListener('click', increment)
+
+
+
+
+decrease.addEventListener('click', decrement)
+
+
+
+
+reset.addEventListener('click', zero)
+
+
+// make value 0
+function zero() {
+    count = 0
+    container.textContent = count
+    if (container.innerHTML == 0) {
+        container.style.color = 'black'
+    }
+
+}
+
+
+
+// increase value
+function increment() {
+
+    count++;
+    container.textContent = count
+
+    if (container.innerHTML > 0) {
+        container.style.color = 'green'
+    }
+
+}
+
+
+
+//decrease value
+function decrement() {
+    count--;
+    container.textContent = count
+
+
+    if (container.innerHTML < 0) {
+        container.style.color = 'red'
+    }
+
+}
+
+
